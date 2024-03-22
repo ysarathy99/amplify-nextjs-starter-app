8b51bad13e70c0bbe19eb3a141fb5b023cb5cfc5
Yathish Sarathy <ysarathy99@gmail.com>
2024-03-22 01:13:03 -0700

    Revert "yarn yarn"
    This reverts commit d3a6747e904d9886fa03b1492fd324092dee8137.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Yz Late Arrival&nbsp;
+          This workz well maann&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
